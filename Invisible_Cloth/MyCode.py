f16350c5a8919162a25beaca15ff7180b26b2cbc
vignesh-creator <60070481+vignesh-creator@users.noreply.github.com>
2020-09-08 23:09:06 +0530

    Add files via upload

---
+++ Invisible_Cloth/MyCode.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2
+out = cv2.VideoWriter('vid.avi',cv2.VideoWriter_fourcc(*'DIVX'),20.0,(640, 480))
+cap = cv2.VideoCapture(0)
+bg=0
+count=0
+if not cap.isOpened():
+    print("Cannot Open Camera")
+    exit()
+i=0
+while(i<30):
+    ret,bg=cap.read()
+    i+=1
+bg = np.flip(bg,axis=1)
+while (True):
+    ret, frame = cap.read()
+    if ret == False:
+        print("Can't receive frame")
+        break
+    count = count + 1
+    frame = np.flip(frame, axis=1)
+    bgr_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower = np.array([94, 80, 2])
+    upper = np.array([126, 255, 255])
+    mask = cv2.inRange(bgr_hsv, lower, upper)
+    bitwise1 = cv2.bitwise_and(frame,frame,mask=cv2.bitwise_not(mask))
+    bitwise2 = cv2.bitwise_and(bg,bg,mask=mask)
+    output = cv2.addWeighted(bitwise1,1,bitwise2,1,0)
+    out.write(output)
+    cv2.imshow("Invisible!", output)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+out.release()
+cv2.destroyAllWindows()
+
---
+++ Invisible_Cloth/README.md
@@ -0,0 +1,4 @@
+# Invisible-Cloth-IVLabs
+The Idea of the project is to write a code which detects the color and replaces it with the background.
+The first While loop is basically to record the background and further,when the video recording appears on the screen,without moving the camera,be seated infront of camera and take a blue cloth and put it on your body
+the blue color in the video is replaced by the background part
